ffb44d469e2495f40e46fef3e88e68ea287ad430
Usemaster <gwj360726@gmail.com>
2021-03-22 22:50:16 +0800

    开始配置

---
+++ Mybatis-study/mybatis-01/src/test/java/com/study/dao/UserDaoTest.java
@@ -0,0 +1,27 @@
+package com.study.dao;
+
+import com.study.pojo.User;
+import com.study.utils.MybatisUtils;
+import org.apache.ibatis.session.SqlSession;
+import org.junit.Test;
+
+import java.util.List;
+
+public class UserDaoTest {
+    @Test
+    public void test(){
+        //第一步获取SqlSession对象
+        SqlSession sqlSesion = MybatisUtils.getSqlSesion();
+
+        //方式一：getMapper
+        UserDao userDao = sqlSesion.getMapper(UserDao.class);
+        List<User> userList = userDao.getUserList();
+
+        for (User user : userList) {
+            System.out.println(user);
+        }
+
+        //关闭SqlSession
+        sqlSesion.close();
+    }
+}
